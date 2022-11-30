e743af4f69d52f7aaa98482a580775d1efffe54d
aka-nk <118865304+aka-nk@users.noreply.github.com>
2022-11-30 14:29:02 -0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,27 @@
+public class Item{
+    private int id;
+    private String nome;
+    private int preço;
+    private int quant;
+    private String descricao;
+    
+
+
+
+    public void usar(Usuario a){
+    }
+
+    
+    public void analisar(){
+      System.out.println(descricao);
+    }
+
+
+
+  public static void main (String[] args) {
+    
+}
+
+
+
+}
